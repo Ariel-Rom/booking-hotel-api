26b9a0a1814c80ba397af687481e06bbfefe65ad
Ariel <arielrom.dev@gmail.com>
2026-07-14 20:45:06 -0300

    feature(repository): agregar metodos a los repositorios de cliente y reserva

--- hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/repository/ClienteRepository.java
+++ hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/repository/ClienteRepository.java
@@ -1,7 +1,13 @@
 package com.spring_boot_ariel_projects.hoteleria.repository;
 
 import com.spring_boot_ariel_projects.hoteleria.model.cliente.Cliente;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Page<Cliente> findClienteByActivoTrue(Pageable pageable);
+    Optional<Cliente> findClienteByActivoTrue(Long clienteId);
 }
--- hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/repository/ReservaRepository.java
+++ hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/repository/ReservaRepository.java
@@ -1,7 +1,13 @@
 package com.spring_boot_ariel_projects.hoteleria.repository;
 
 import com.spring_boot_ariel_projects.hoteleria.model.reserva.Reserva;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    Page<Reserva> findReservaByActivoTrue(Pageable pagination);
+    Optional<Reserva> findReservaByActivoTrue(Long id);
 }
