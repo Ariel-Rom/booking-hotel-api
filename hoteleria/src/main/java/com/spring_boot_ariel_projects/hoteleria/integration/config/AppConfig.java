e6aa3a1e79016c5649abbad5a054397fc82df2f2
Ariel <arielrom.dev@gmail.com>
2026-07-17 15:07:15 -0300

    feat(integration): configurar API Key desde application.yml

--- hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/integration/config/AppConfig.java
+++ hoteleria/src/main/java/com/spring_boot_ariel_projects/hoteleria/integration/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.spring_boot_ariel_projects.hoteleria.integration.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,14 +8,15 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class AppConfig {
 
-    private static final String API_KEY = "eec02f9761ec638b79700412290520134fdcf8678ac2ca442d9b3754a4d94e8f";
+    @Value("${api.hotels.key}")
+    private String apiKey;
 
     @Bean
     RestClient restClient(){
         RestClient.Builder builder = RestClient.builder();
         return builder
                 .baseUrl("https://api.hotels-api.com/v1")
-                .defaultHeader("X-API-Key", API_KEY)
+                .defaultHeader("X-API-Key", apiKey)
                 .build();
     }
 
